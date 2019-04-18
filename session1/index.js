473bd0f0c1c796527b70646750569875e895f11e
AnhQuann <anhquan19051905@gmail.com>
2019-04-18 22:13:38 +0700

    .

---
+++ session1/index.js
@@ -0,0 +1,24 @@
+// let name = prompt("Enter your name? ");
+// console.log("Hello", name);
+
+// const x = prompt("Nhap vao so thu 1");
+// const y = prompt("Nhap vao so thu 2");
+
+// console.log(Number(x) + Number(y));
+
+// 1. Tạo file html
+// 2. Gõ html, chọn html:5 để hiển thị khung html
+// 3. Tạo file js để code js
+// 4. Nối 2 file với nhau thông qua script
+// 5. Chạy file html
+// 6. Inspect (Kiểm tra) trên trình duyệt (chrome)
+// 7. Chuyển qua tab Console để xem kết quả
+
+
+// Loops
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+  console.log("Hello");
+}
+
+// ++ tăng thêm 1 đơn vị
