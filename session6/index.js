92e7fb7252e32331cbb096b6801283f0b3048032
AnhQuann <anhquan19051905@gmail.com>
2019-07-18 20:14:40 +0700

    .

--- session6/index.js
+++ session6/index.js
@@ -1,10 +1,10 @@
-// console.log(1);
-
-// setTimeout(() => {
-//   console.log(2);
-// }, 3000);
-
-// console.log(3);
+// window.onload = async () => {
+//   console.log(1);
+  
+//   setTimeout('', 3000);
+  
+//   console.log(3);
+// }
 
 // async / await
 
